ae3dbca12bc83e4bb44f039a0c543899ab0c223e
DEATHAIsol <catalyst.labs0@gmail.com>
2025-06-25 11:50:25 +1000

    tracer addition1

--- src/lib/components/modals/menu.svelte
+++ src/lib/components/modals/menu.svelte
@@ -2,12 +2,7 @@
     import Network from "../network.svelte";
 </script>
 
-{#each [
-    { name: "Home", url: "/" },
-    { name: "Dexscreener", url: "https://dexscreener.com" },
-    { name: "Twitter", url: "https://x.com" },
-    { name: "Discord", url: "https://discord.com" }
-] as { name, url }}
+{#each [{ name: "Home", url: "/" }, { name: "Dexscreener", url: "https://dexscreener.com" }, { name: "Twitter", url: "https://x.com" }, { name: "Discord", url: "https://discord.com" }] as { name, url }}
     <a
         href={url}
         data-sveltekit-reload
